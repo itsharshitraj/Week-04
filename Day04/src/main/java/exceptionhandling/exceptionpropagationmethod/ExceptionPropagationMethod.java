982fc3f9caef4525a79b3402d9204af747aaffd2
Harshit <itsharshitraj@gmail.com>
2025-02-14 21:23:35 +0530

    [Harshit] Add . Exception Propagation in Methods

---
+++ Day04/src/main/java/exceptionhandling/exceptionpropagationmethod/ExceptionPropagationMethod.java
@@ -0,0 +1,19 @@
+package exceptionhandling.exceptionpropagationmethod;
+
+public class ExceptionPropagationMethod {
+    static void method1() {
+        throw new ArithmeticException("Division by zero");
+    }
+
+    static void method2() {
+        method1();
+    }
+
+    public static void main(String[] args) {
+        try {
+            method2();
+        } catch (ArithmeticException e) {
+            System.out.println("Handled exception in main");
+        }
+    }
+}
