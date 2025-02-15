6395a66e76a03a59eb4b68524988f7f67d6a1850
Harshit <itsharshitraj@gmail.com>
2025-02-15 15:41:39 +0530

    [Harshit] Add . Extract Links from Webpage

---
+++ Day05/src/main/java/regex/extractlinksfromwebpage/ExtractLinks.java
@@ -0,0 +1,18 @@
+package regex.extractlinksfromwebpage;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractLinks {
+    public static void main(String[] args) {
+        String text = "Visit https://www.google.com and http://example.org for more info.";
+        String regex = "https?://[^\\s]+";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        while(matcher.find()){
+            System.out.println("Output : " + matcher.group());
+        }
+    }
+}
